b68483c08cfe574ce081de7512fdeb7d793af570
Prakash Mohan <prakash.mohan@koboldmetals.com>
2026-03-04 16:29:40 -0700

    cleanup unused plugings, and make which-key less aggressive

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,19 +1,12 @@
 return {
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter',
-    ---@module 'which-key'
-    ---@type wk.Opts
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      delay = 0,
-      icons = { mappings = vim.g.have_nerd_font },
-      spec = {
-        { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        { 'gr', group = 'LSP Actions', mode = { 'n' } },
-      },
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  keys = {
+    {
+      '<leader>?',
+      function() require('which-key').show { global = false } end,
+      desc = 'Buffer Local Keymaps (which-key)',
     },
   },
 }

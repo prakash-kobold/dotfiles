b19fcd471b644f483803acee808bf27910be357b
Prakash Mohan <prakash.mohan@koboldmetals.com>
2026-03-03 18:25:29 -0700

    add cli deps

---
+++ run_before_install_00-packages-cli.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+fi
+
+if [[ ! -x "$(command -v agent)" ]]; then
+    curl -fsS https://cursor.com/install | bash
+fi
+
+if [[ ! -x "$(command -v claude)" ]]; then
+    curl -fsSL https://claude.ai/install.sh | bash
+fi
